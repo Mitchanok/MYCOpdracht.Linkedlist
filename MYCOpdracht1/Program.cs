437e3ed13aaad945c1df9fad8823bf39d20a6b26
Mitchanok <94455227+Mitchanok@users.noreply.github.com>
2024-02-21 09:46:16 +0100

    ..

--- MYCOpdracht1/Program.cs
+++ MYCOpdracht1/Program.cs
@@ -1,3 +1,20 @@
-﻿
+﻿//Vragen
 
-//
+//1.	Kan je voor elke method aangeven wat de orde (grote O) is?
+//void Add() O(1)
+//void Clear() O(1)
+//int IndexOf() O(n)
+//bool COntains() O(n)
+//void Insert() O(n)
+//void Remove() O(n)
+//void RemoveAt() O(n)
+//T this[] O(n)
+//int Count() O(1)
+//2.	Zou je nog andere data structuren kunnen noemen die deze interface ook zouden kunnen implementeren?
+//Der zijn nog andere ZOals Arrays, Stacks en ques
+//3.	Waarom zou je dit willen?
+// Het gerbuik van de interface maakt het makkelijk voor flexibiliteit en herbruikbaarheid van de code door verschillende implementaties
+// uitwisselbaar te maken wat het makkelijker maakt om de code te testen 
+//4.	Wat zijn de voordelen van generics in dit voorbeeld?
+//Met het gebruik van generics is het makkelijk om de herbruikbaarheid van de code te gebruiken 
+
